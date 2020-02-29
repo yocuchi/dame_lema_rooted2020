711bcdbdf0b6935739852773757d78318ce4d340
Francisco Hernández Cuchí <yocuchi@gmail.com>
2020-02-29 16:31:48 +0100

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?
-
-$settings = array(
-    'oauth_access_token' => "Ese toro enamorado de la luna",
-    'oauth_access_token_secret' => "que abandonda por la noche la mana",
-    'consumer_key' => "abanicos de colores",
-    'consumer_secret' => "parecen sus patas"
-);
-?>
