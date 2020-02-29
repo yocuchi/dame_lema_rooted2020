a662864d257451519a5c55b90edc83089d32abc1
Francisco Hernández Cuchí <yocuchi@gmail.com>
2020-02-29 16:33:08 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?
+  
+$settings = array(
+    'oauth_access_token' => "put your balls here",
+    'oauth_access_token_secret' => "",
+    'consumer_key' => "",
+    'consumer_secret' => ""
+);
+?>
