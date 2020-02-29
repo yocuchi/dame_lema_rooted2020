72d54b19a0e8f719a24da7595d6e70863a64b218
Francisco Hernández Cuchí <yocuchi@gmail.com>
2020-02-29 16:29:07 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?
 
 $settings = array(
-    'oauth_access_token' => "24668301-K2TusXio21D8L66eA5ORJdH9HlZDIMhRxKzCml3Ct",
-    'oauth_access_token_secret' => "SSeyq7K9XJnq9GeoGK63MQqIyTTxV6npK5H9mjC5BVEzB",
-    'consumer_key' => "PHj71J1L3co15sMOB56lSQ",
-    'consumer_secret' => "DxtNRygCbEh7T1tcWfBscu95skENQkJPvAKtRanBaE"
+    'oauth_access_token' => "Ese toro enamorado de la luna",
+    'oauth_access_token_secret' => "que abandonda por la noche la mana",
+    'consumer_key' => "abanicos de colores",
+    'consumer_secret' => "parecen sus patas"
 );
 ?>
