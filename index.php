1def48d46209aa093532dda129c26cd369f6f2ad
root <root@56236.s.time4vps.cloud>
2020-03-05 16:37:00 +0100

    post rooted

--- index.php
+++ index.php
@@ -36,30 +36,62 @@ foreach($string["statuses"] as $items)
 $array_nombres = array("Gañán", "Pataliebre", "Tolai", "pequeño saltamontes", "hacker de gameboys");
 $s=array_rand($hits,2);
 $msg="Recuerda ". $array_nombres[(array_rand($array_nombres))] . ",<br/>  " . $hits[$s[0]] . " no es un " . $hits[$s[1]];
+$msg="Recuerda ". $array_nombres[(array_rand($array_nombres))] . ",<br/>  " . htmlspecialchars($hits[$s[0]]) . " no es un " . htmlspecialchars($hits[$s[1]]);
 
+
+//$msg=htmlspecialchars($msg);
 
 ?>
+
 <html>
 	<head>
-		<link href="https://fonts.googleapis.com/css?family=Tomorrow&display=swap" rel="stylesheet">
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+		<link href="http://fonts.googleapis.com/css?family=Tomorrow&display=swap" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="./css/style.css" />
+        <link rel="stylesheet" type="text/css" href="./css/font-awesome.css" />
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 		<script>
+			
 			$(document).ready(function(){
-				$( ".boton" ).click(function() {
-					$( ".boton" ).fadeOut("slow");
-					var image = Math.floor(Math.random()*4)+1;
+				function imgRand(pulsado) {
+					if(pulsado==1){
+					$( ".boton" ).fadeOut();
+						$( ".resultado" ).fadeIn(1300);
+					}else{
+						$( ".boton" ).fadeOut(1500);
+						$( ".resultado" ).delay(1600).fadeIn(2000);
+					}
+					var image = Math.floor(Math.random()*7)+1;
 					if(image==1){
-						$( ".resultado" ).css("background","url(https://images.pexels.com/photos/32237/pexels-photo.jpg)");
+						$( ".resultado" ).css("background","url(img/Stars.jpg)");
 					}else if(image==2){
-						$( ".resultado" ).css("background","url(https://images.pexels.com/photos/169647/pexels-photo-169647.jpeg)");
+						$( ".resultado" ).css("background","url(img/pexels-photo-169647.jpeg)");
 					}else if(image==3){
-						$( ".resultado" ).css("background","url(https://images.pexels.com/photos/1434608/pexels-photo-1434608.jpeg)");
+						$( ".resultado" ).css("background","url(img/pexels-photo-1434608.jpeg)");
 					}else if(image==4){
-						$( ".resultado" ).css("background","url(https://3.bp.blogspot.com/-auaLS9Hlre8/VZLCd85ycGI/AAAAAAAABYI/Efqt7Zvx-OM/s1600/YRBRRRI.png)");
+						$( ".resultado" ).css("background","url(img/YRBRRRI.png)");
+					}else if(image==5){
+						$( ".resultado" ).css("background","url(img/06bc1bbd29c978e2a3ca5a4ab73d9dc7.jpg)");
+					}else if(image==6){
+						$( ".resultado" ).css("background","url(img/1518607998_924815_1518608760_noticia_normal.jpg)");
+					}else if(image==7){
+						$( ".resultado" ).css("background","url(img/k2_46924cb7_1500x978.jpg)");
 					}
 					
-					$( ".resultado" ).delay(800).fadeIn(2000);
+					
+			
+				};
+				
+				if(window.location=='index.php?pulsado=1'){
+					imgRand(1);
+				}
+				
+				$( ".boton" ).click(function() {
+					imgRand(0);
 				});
+				$( ".frase div" ).click(function() {
+					$(location).attr('href','index.php?pulsado=1');
+				});
+			
 			});
 		</script>
 		<style>
@@ -71,18 +103,22 @@ $msg="Recuerda ". $array_nombres[(array_rand($array_nombres))] . ",<br/>  " . $h
 			}
 			a{
 				text-decoration: none;
-				padding: 35px;
+				//padding: 35px;
 				font-weight: 600;
 				font-size: 28px;
-				color: #ffffff;
-				background-color: #1883ba;
-				border-radius: 6px;
-				border: 2px solid #0016b0;
+				//color: #ffffff;
+				//background-color: #1883ba;
+				//border-radius: 6px;
+				//border: 2px solid #0016b0;
 				font-family: 'Tomorrow', sans-serif;
+			}
+			a img{
+				width: 86%;
+				margin-top: 7%;
 			}
 
 			.boton{
-				    margin-top: 21%;
+				    margin-top: 260px;
 			}
 			
 			 a:hover{
@@ -91,7 +127,7 @@ $msg="Recuerda ". $array_nombres[(array_rand($array_nombres))] . ",<br/>  " . $h
 				
 			 }
 			.frase{
-				padding-top: 18%;
+				padding-top: 17%;
 				font-family: 'Tomorrow', sans-serif;
 				color:white;
 				font-size:80px;
@@ -99,11 +135,12 @@ $msg="Recuerda ". $array_nombres[(array_rand($array_nombres))] . ",<br/>  " . $h
 			.frase div{
 				width: 100%;
 				background-color: rgba(0, 0, 0, 0.6);
+				padding-top: 2%;
+				padding-bottom: 2%;
 				
 			}
 			.resultado{
 				display:none;
-				//background:url(https://images.pexels.com/photos/32237/pexels-photo.jpg);
 				background-size:100% 100% !important;
 				width:100% !important;
 				height:100% !important;
@@ -115,9 +152,14 @@ $msg="Recuerda ". $array_nombres[(array_rand($array_nombres))] . ",<br/>  " . $h
 	<body>
 		<div class="resultado">
 			<div class="frase">
-			<div><b><?=$msg ?></b></div>
+				<div><b>
+
+<?=$msg ?>
+</b></div>
 		</div>
 		</div>
-		<div class="boton"><a href="#">Pulsa aquÃ­ ðŸ˜‰</a></div>
+		<div class="boton"><a href="#"><div class="switch demo4">
+					<input type="checkbox">
+					<label><img src="img/nuke.png"/></label>
+				</div></a></div>
 	</body>
-</html>
